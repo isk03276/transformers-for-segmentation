721b11e4ac8f0b64cd2fea570040a599eceea48a
isk03276 <isk03276@naver.com>
2022-09-22 16:21:31 +0900

    Add 3d image splitting

--- tests/utils/test_image.py
+++ tests/utils/test_image.py
@@ -12,10 +12,13 @@ except ModuleNotFoundError:
 
 class TestImage(unittest.TestCase):
     def test_slice_image_to_patches(self):
-        images = torch.zeros((10, 3, 200, 200))
+        images_2d = torch.zeros((10, 3, 200, 200))
+        images_3d = torch.zeros((10, 200, 200, 200))
         patch_size = 2
-        patches = slice_image_to_patches(images, patch_size)
-        assert patches.shape == (10, 10000, 3 * patch_size * patch_size)
+        patches_2d = slice_image_to_patches(images_2d, patch_size, flatten=True, is_3d_data=False)
+        patches_3d = slice_image_to_patches(images_3d, patch_size, flatten=True, is_3d_data=True)
+        assert patches_2d.shape == (10, 10000, 3 * patch_size * patch_size)
+        assert patches_3d.shape == (10, 1000000, patch_size**3)
 
 
 if __name__ == "__main__":
--- utils/image.py
+++ utils/image.py
@@ -10,7 +10,7 @@ def pad_image(image: Union[np.ndarray, torch.Tensor]):
 
 
 def slice_image_to_patches(
-    images: torch.Tensor, patch_size: int, flatten: bool = True
+    images: torch.Tensor, patch_size: int, flatten: bool = True, is_3d_data: bool = True,
 ) -> torch.Tensor:
     """
     Split images into patches.
@@ -21,17 +21,26 @@ def slice_image_to_patches(
 
     images_shape = images.shape
     n_batch, n_channel = images_shape[:2]
-    patches = (
-        images.unfold(1, n_channel, n_channel)
-        .unfold(2, patch_size, patch_size)
-        .unfold(3, patch_size, patch_size)
-        .squeeze(dim=1)
-    )
-    if flatten:
-        patches = patches.flatten(start_dim=1, end_dim=2)
-        patches = patches.flatten(start_dim=2)
+    if is_3d_data:
+        patches = (
+            images.unfold(1, patch_size, patch_size)
+            .unfold(2, patch_size, patch_size)
+            .unfold(3, patch_size, patch_size)
+        )
+        if flatten:
+            patches = patches.flatten(start_dim=1, end_dim=3)
+            patches = patches.flatten(start_dim=2)
+    else:
+        patches = (
+            images.unfold(1, n_channel, n_channel)
+            .unfold(2, patch_size, patch_size)
+            .unfold(3, patch_size, patch_size)
+            .squeeze(dim=1)
+        )
+        if flatten:
+            patches = patches.flatten(start_dim=1, end_dim=2)
+            patches = patches.flatten(start_dim=2)
     return patches
-
 
 def load_from_nii(path_to_load: str)-> Tuple[np.ndarray, dict]:
     """
