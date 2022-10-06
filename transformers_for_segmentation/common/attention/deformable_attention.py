9e91d130b9c328b2bcabfa32b374664fa2c14c72
isk03276 <isk03276@naver.com>
2022-10-06 09:34:51 +0900

    Modify comments

--- transformers_for_segmentation/common/attention/deformable_attention.py
+++ transformers_for_segmentation/common/attention/deformable_attention.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 from transformers_for_segmentation.common.attention.multi_head_self_attention import MultiHeadSelfAttention
 
@@ -6,4 +7,30 @@ from transformers_for_segmentation.common.attention.multi_head_self_attention im
 class DeformableAttention(MultiHeadSelfAttention):
     def __init__(self, n_dim:int, n_heads: int):
         super().__init__(n_dim=n_dim, n_heads=n_heads)
+        self.offset_net = OffsetNet(n_dim=n_dim)
         
+    def forward(self, x):
+        queries = self.split_multiple_heads(self.query(x))
+        offset = self.offset_net(queries)
+        
+        
+        
+        keys = self.split_multiple_heads(self.key(x))
+        values = self.split_multiple_heads(self.value(x))
+
+        result = self.scaled_dot_proudction(query=queries, key=keys, value=values)
+        result = self.merge_multiple_heads(result)
+        result = self.linear(result)
+        return result
+        
+        
+class OffsetNet(nn.Module):
+    def __init__(self, n_dim:int, n_offset:int, n_heads:int, kernel_size: int):
+        super().__init__()
+        self.net = nn.Sequential(nn.Conv3d(in_channels=n_dim, out_channels=n_dim, kernel_size=kernel_size, ),
+                                 nn.GELU(),
+                                 nn.Conv3d(in_channels=n_dim, out_channels=3, kernel_size=1),
+                                 nn.Tanh(),
+        )
+    def forward(self, x):
+        return self.net(x)
--- transformers_for_segmentation/common/attention/multi_head_self_attention.py
+++ transformers_for_segmentation/common/attention/multi_head_self_attention.py
@@ -33,18 +33,18 @@ class MultiHeadSelfAttention(nn.Module):
         assert len(tensor.size()) == 3
 
         batch_size, seq_len, emb_dim = tensor.size()
-        # (B, N, Emb) -> (B, N, n_head, splitted_dim)
+        # (B, SeqN, Emb) -> (B, SeqN, n_head, splitted_dim)
         tensor = tensor.reshape(
             batch_size, seq_len, self.n_heads, emb_dim // self.n_heads
         )
-        # (B, N, n_head, splitted_dim) -> (B, n_head, N, splitted_dim)
+        # (B, SeqN, n_head, splitted_dim) -> (B, n_head, SeqN, splitted_dim)
         tensor = tensor.transpose(1, 2)
         return tensor
 
     def scaled_dot_proudction(
         self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor
     ):
-        # (B, n_head, N, splitted_dim)
+        # (B, n_head, SeqN, splitted_dim)
         transposed_key = key.transpose(-2, -1)
         attention_score_map = torch.matmul(query, transposed_key) * self.scaling_factor
         attention_prob_map = self.softmax(attention_score_map)
@@ -55,8 +55,8 @@ class MultiHeadSelfAttention(nn.Module):
         assert len(tensor.size()) == 4
 
         batch_size, n_heads, seq_len, splitted_dim = tensor.size()
-        # (B, n_head, N, splitted_dim) -> (B, N, n_head, splitted_dim)
+        # (B, n_head, SeqN, splitted_dim) -> (B, SeqN, n_head, splitted_dim)
         tensor = tensor.transpose(1, 2)
-        # (B, N, n_head, splitted_dim) -> (B, N, n_head * splitted_dim)
+        # (B, SeqN, n_head, splitted_dim) -> (B, SeqN, n_head * splitted_dim)
         tensor = tensor.reshape(batch_size, seq_len, n_heads * splitted_dim)
         return tensor
