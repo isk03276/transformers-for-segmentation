64d2143695884035b123c06404f77a834381797f
isk03276 <isk03276@naver.com>
2022-10-06 09:41:33 +0900

    Move method position

--- transformers_for_segmentation/unetr/layers.py
+++ transformers_for_segmentation/unetr/layers.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 
+from transformers_for_segmentation.common.attention.multi_head_self_attention import MultiHeadSelfAttention
+from transformers_for_segmentation.common.layers import MLPBlock
 
 class Deconv3DLayer(nn.Module):
     def __init__(
@@ -98,4 +100,22 @@ class Deconv3dBlock(nn.Module):
     def forward(self, x):
         x = self.deconv_layer(x)
         x = self.conv_block(x)
+        return x
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, n_dim: int, n_heads: int):
+        super().__init__()
+        self.layer_norm = nn.LayerNorm(n_dim)
+        self.multi_head_attention = MultiHeadSelfAttention(n_dim=n_dim, n_heads=n_heads)
+        self.mlp_block = MLPBlock(n_dim=n_dim)
+
+    def forward(self, x):
+        x_backup = x
+        x = self.layer_norm(x)
+        x = self.multi_head_attention(x)
+        x = x_backup = x + x_backup
+        x = self.layer_norm(x)
+        x = self.mlp_block(x)
+        x += x_backup
         return x
