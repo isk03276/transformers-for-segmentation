aada4f921363917240ae8ea1205ea9a79571561e
isk03276 <isk03276@naver.com>
2022-10-11 13:16:01 +0900

    Modify reference points method

--- transformers_for_segmentation/common/attention/deformable_attention.py
+++ transformers_for_segmentation/common/attention/deformable_attention.py
@@ -48,58 +48,47 @@ class DeformableAttention(MultiHeadSelfAttention):
             queries,
             "b (g c) d h w  -> (b g) c d h w",
             g=self.n_groups,
-            c=self.n_group_dim,
         )
         offset = self.offset_net(grouped_queries)
-        n_depth, n_height, n_width = offset.shape[2:]
-        n_offset_samples = n_depth * n_height * n_width
         offset = einops.rearrange(offset, "b p d h w -> b d h w p")
-
-        ref_points = self.get_ref_points(
-            n_depth, n_height, n_width, batch_size, x.dtype, x.device
-        )
-        pos = (offset + ref_points).tanh()
-
+        ref_points = self.create_grid_like(x)
+        ref_points = einops.rearrange(ref_points, 'p d h w -> d h w p')
+        deformed_points = (offset + ref_points).tanh()
+        
         x_sampled = F.grid_sample(
-            input=einops.rearrange(
+            einops.rearrange(
                 x,
                 "b (g gc) d h w -> (b g) gc d h w",
                 g=self.n_groups,
-                gc=self.n_group_dim,
             ),
-            grid=pos[..., (2, 1, 0)],  # z, y, x  -> x, y, z
+            deformed_points,
             mode="bilinear",
-            align_corners=True,
+            padding_mode="zeros",
+            align_corners=False,
         )
-        x_sampled = x_sampled.reshape(batch_size, n_dim, 1, 1, n_offset_samples)
+        x_sampled = einops.rearrange(x_sampled, "(b g) d ... -> b (g d) ...", b = batch_size)
 
         queries = queries.reshape(
             batch_size * self.n_heads, self.n_head_dim, depth * height * width
         )
         keys = self.key(x_sampled).reshape(
-            batch_size * self.n_heads, self.n_head_dim, n_offset_samples
+            batch_size * self.n_heads, self.n_head_dim, depth * height * width
         )
         values = self.value(x_sampled).reshape(
-            batch_size * self.n_heads, self.n_head_dim, n_offset_samples
+            batch_size * self.n_heads, self.n_head_dim, depth * height * width
         )
 
         result = self.scaled_dot_proudction(query=queries, key=keys, value=values)
         result = result.reshape(batch_size, n_dim, depth, height, width)
         return result
 
-    @torch.no_grad()
-    def get_ref_points(self, d_key, h_key, w_key, batch_size, dtype, device):
-        ref_z, ref_y, ref_x = torch.meshgrid(
-            torch.linspace(0.5, d_key - 0.5, d_key, dtype=dtype, device=device),
-            torch.linspace(0.5, h_key - 0.5, h_key, dtype=dtype, device=device),
-            torch.linspace(0.5, w_key - 0.5, w_key, dtype=dtype, device=device),
-        )
-        ref = torch.stack((ref_z, ref_y, ref_x), -1)
-        ref[..., 2].div_(w_key).mul_(2).sub_(1)
-        ref[..., 1].div_(h_key).mul_(2).sub_(1)
-        ref[..., 0].div_(d_key).mul_(2).sub_(1)
-        ref = ref[None, ...].expand(
-            batch_size * self.n_groups, -1, -1, -1, -1
-        )  # B * g H W 2
-
-        return ref
+    def create_grid_like(self, tensor, dim = 0):
+        depth, height, width, device = *tensor.shape[2:], tensor.device
+        grid = torch.stack(torch.meshgrid(
+            torch.arange(depth, device=device),
+            torch.arange(height, device=device),
+            torch.arange(width, device=device),
+        ), dim=dim)
+        grid.requires_grad = False
+        grid = grid.type_as(tensor)
+        return grid
