f9614f41628d4ae0dc4c1767c8e471e906f517e4
isk03276 <isk03276@naver.com>
2022-10-24 15:41:48 +0900

    Add numpy utils

---
+++ utils/numpy.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+import numpy as np
+
+
+def np_where_for_multi_dim_array(array_condition)-> np.ndarray:
+    """
+    Get index of a value in multi-dimension array.
+    The 'array' arg must be an array of two or more dimensions.
+    """
+    return np.array(list(zip(*np.where(array_condition))))
